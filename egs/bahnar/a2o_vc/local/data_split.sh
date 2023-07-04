e1aa4244986fdba47c99fe843da3417873528001
tqtensor <victortang212@gmail.com>
2023-07-04 06:10:52 +0000

    chore: adjust training settings to fit one A100

--- egs/bahnar/a2o_vc/local/data_split.sh
+++ egs/bahnar/a2o_vc/local/data_split.sh
@@ -15,8 +15,8 @@ eval_list="${lists_dir}/eval_list.txt"
 # list all WAV files in the directory and shuffle the list
 wav_files=$(find "$wav_directory" -type f -name "*.wav" | shuf)
 
-# downsample to 150 files
-wav_files=$(echo "$wav_files" | head -n 150)
+# downsample to 25 files
+wav_files=$(echo "$wav_files" | head -n 25)
 
 # calculate the number of files for each set
 total_files=$(echo "$wav_files" | wc -l)
