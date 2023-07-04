12e1960e76727033f823a6bf763d2a65f670716a
tqtensor <victortang212@gmail.com>
2023-07-04 01:40:47 +0000

    chore: update data prep based on a100

--- egs/bahnar/a2o_vc/local/data_split.sh
+++ egs/bahnar/a2o_vc/local/data_split.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
-# Directory containing the WAV files
-wav_directory="./downloads/bahnar/MGL1"
+db_root=$1
+spk=$2
+lists_dir=$3
 
-# Output filenames
-train_list="./local/lists/train_list.txt"
-dev_list="./local/lists/dev_list.txt"
-eval_list="./local/lists/eval_list.txt"
+# directory containing the WAV files
+wav_directory="${db_root}/${spk}"
 
-# List all WAV files in the directory and shuffle the list
+# output filenames
+train_list="${lists_dir}/train_list.txt"
+dev_list="${lists_dir}/dev_list.txt"
+eval_list="${lists_dir}/eval_list.txt"
+
+# list all WAV files in the directory and shuffle the list
 wav_files=$(find "$wav_directory" -type f -name "*.wav" | shuf)
 
-# Downsample to 150 files
+# downsample to 150 files
 wav_files=$(echo "$wav_files" | head -n 150)
 
-# Calculate the number of files for each set
+# calculate the number of files for each set
 total_files=$(echo "$wav_files" | wc -l)
 train_count=$((total_files * 80 / 100))
 dev_count=$((total_files * 10 / 100))
 eval_count=$((total_files - train_count - dev_count))
 
-# Extract the file names for each set
+# extract the file names for each set
 train_files=$(echo "$wav_files" | head -n $train_count | xargs -I {} basename {} .wav)
 dev_files=$(echo "$wav_files" | tail -n +$((train_count + 1)) | head -n $dev_count | xargs -I {} basename {} .wav)
 eval_files=$(echo "$wav_files" | tail -n $eval_count | xargs -I {} basename {} .wav)
 
-# Write the file names to their respective output files
+# write the file names to their respective output files
 echo "$train_files" >"$train_list"
 echo "$dev_files" >"$dev_list"
 echo "$eval_files" >"$eval_list"
